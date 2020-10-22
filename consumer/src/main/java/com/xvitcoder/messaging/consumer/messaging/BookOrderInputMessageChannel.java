4f33d05102b37fe79965c346b16bc8e47021d9ec
Vitalie Mudrenco <xvitcoder@gmail.com>
2020-10-22 17:30:26 +0300

    Configured the consumer group

--- consumer/src/main/java/com/xvitcoder/messaging/consumer/messaging/BookOrderInputMessageChannel.java
+++ consumer/src/main/java/com/xvitcoder/messaging/consumer/messaging/BookOrderInputMessageChannel.java
@@ -1,11 +1,11 @@
 package com.xvitcoder.messaging.consumer.messaging;
 
 import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 public interface BookOrderInputMessageChannel {
     String INPUT = "bookOrderInput";
 
     @Input(BookOrderInputMessageChannel.INPUT)
-    MessageChannel input();
+    SubscribableChannel input();
 }
--- publisher/src/main/java/com/xvitcoder/messaging/publisher/messaging/BookProcessInputMessageChannel.java
+++ publisher/src/main/java/com/xvitcoder/messaging/publisher/messaging/BookProcessInputMessageChannel.java
@@ -1,11 +1,11 @@
 package com.xvitcoder.messaging.publisher.messaging;
 
 import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 public interface BookProcessInputMessageChannel {
     String INPUT = "bookProcessInput";
 
     @Input(BookProcessInputMessageChannel.INPUT)
-    MessageChannel input();
+    SubscribableChannel input();
 }
